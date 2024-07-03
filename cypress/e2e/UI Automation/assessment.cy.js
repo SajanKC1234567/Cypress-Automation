e750f8dde535defd3e190dc752737b73e956a128
SajanKC1234567 <142201424+SajanKC1234567@users.noreply.github.com>
2024-07-03 10:27:33 +0545

    Update assessment.cy.js

--- cypress/e2e/UI Automation/assessment.cy.js
+++ cypress/e2e/UI Automation/assessment.cy.js
@@ -1,40 +1,40 @@
-// describe('registration', () => {
-//   it('register', () => {
-//     cy.registration()
-//     cy.wait(1000)
-//   })
+describe('registration', () => {
+  it('register', () => {
+    cy.registration()
+    cy.wait(1000)
+  })
 
-//   it('login', () => {
-//     cy.login()
-//     cy.wait(1000)
-//   })
+  it('login', () => {
+    cy.login()
+    cy.wait(1000)
+  })
 
-//   it('update profile', () => {
-//     cy.updateprofile()
-//     cy.wait(1000)
-//   })
+  it('update profile', () => {
+    cy.updateprofile()
+    cy.wait(1000)
+  })
 
-//   it('add notes', () => {
-//     cy.addNote()
-//     cy.wait(1000)
-//   })
+  it('add notes', () => {
+    cy.addNote()
+    cy.wait(1000)
+  })
 
-//   it('edit notes', () => {
-//     cy.editNote()
-//     cy.wait(1000)
-//   })
+  it('edit notes', () => {
+    cy.editNote()
+    cy.wait(1000)
+  })
 
-//   it('delete notes', () => {
-//     cy.deleteNote()
-//     cy.wait(1000)
-//   })
+  it('delete notes', () => {
+    cy.deleteNote()
+    cy.wait(1000)
+  })
 
-//   it('logout', () => {
-//     cy.destroySession()
-//     cy.wait(1000)
-//   })
+  it('logout', () => {
+    cy.destroySession()
+    cy.wait(1000)
+  })
 
-//   it('delete account', () => {
-//     cy.deleteAccount()
-//   })
-// })
+  it('delete account', () => {
+    cy.deleteAccount()
+  })
+})
