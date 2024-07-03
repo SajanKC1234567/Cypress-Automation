9e938aca94e095aeec30ddcdbe50ed170c883a9f
SajanKC1234567 <142201424+SajanKC1234567@users.noreply.github.com>
2024-07-03 10:28:04 +0545

    Update assessmentAPI.cy.js

--- cypress/e2e/API Automation/assessmentAPI.cy.js
+++ cypress/e2e/API Automation/assessmentAPI.cy.js
@@ -3,38 +3,4 @@ describe('registration', () => {
     cy.apiUserRegistration()
     cy.wait(1000)
   })
-
-  // it('login', () => {
-  //   cy.login()
-  //   cy.wait(1000)
-  // })
-
-  // it('update profile', () => {
-  //   cy.updateprofile()
-  //   cy.wait(1000)
-  // })
-
-  // it('add notes', () => {
-  //   cy.addNote()
-  //   cy.wait(1000)
-  // })
-
-  // it('edit notes', () => {
-  //   cy.editNote()
-  //   cy.wait(1000)
-  // })
-
-  // it('delete notes', () => {
-  //   cy.deleteNote()
-  //   cy.wait(1000)
-  // })
-
-  // it('logout', () => {
-  //   cy.destroySession()
-  //   cy.wait(1000)
-  // })
-
-  // it('delete account', () => {
-  //   cy.deleteAccount()
-  // })
 })
